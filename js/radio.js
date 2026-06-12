575273ec931ab0d7a863273131f0f21cc52851f2
Nutakoe000 <vindox88@gmail.com>
2026-06-12 12:39:47 +0500

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -1,9 +1,9 @@
 const tracks = [
     'audio/Ruth_Etting_-_Body_And_Soul.mp3', 
-    'audio/Vera-Lynn-Well-Meet-Again.mp3',
+    'audio/Vera_Lynn_-_Well_Meet_Again_(mp3.pm).mp3',
     'audio/youre-never-fully-dressed-without-a-smile.mp3',
-    'audio/But-I-Do-You-Know-I-Do.mp3',
-    'audio/Player-Baby-Come-Back-PlayerVEVO.mp3', 
+    'audio/Ruth_Etting_-_But_I_Do_You_Know_I_Do_(mp3.pm).mp3',
+    'audio/Player_-_Baby_Come_Back_(mp3.pm).mp3', 
 ];
 
 let currentTrack = 0;
